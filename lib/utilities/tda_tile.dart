ca4e6dd0cd5fd917f000691a883f323d0a4e7441
KennOtieno <otiken001@gmail.com>
2025-05-21 06:56:41 -0700

    New Changes

---
+++ lib/utilities/tda_tile.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class TdaTile extends StatelessWidget {
+  const TdaTile({super.key});
+}
+
+@override
+Widget build(BuildContext context) {
+  return Container(
+    child: Text('Prepare a List'),
+    decoration: BoxDecoration(color: Colors.blueGrey),
+  );
+}
