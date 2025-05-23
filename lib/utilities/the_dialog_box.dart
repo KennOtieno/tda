26808be65363914df7ff391aecaba02e0e5336d3
KennOtieno <otiken001@gmail.com>
2025-05-23 05:36:09 -0700

    New Changes

--- lib/utilities/the_dialog_box.dart
+++ lib/utilities/the_dialog_box.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:tda/utilities/buttons.dart';
 
 class TheDialogBox extends StatelessWidget {
-  const TheDialogBox({super.key});
+  // ignore: prefer_typing_uninitialized_variables
+  final controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+  const TheDialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+    });
 }
 
 @override
 Widget build (BuildContext context) {
   return AlertDialog(
     backgroundColor: Colors.blueGrey,
-    content: Container(
+    content: SizedBox(
       height: 200,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           // A Text Field o get User Input
           TextField(
+            controller: controller,
             decoration: InputDecoration(
               hintText: 'Create a New Task',
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(12),
-                borderSide: BorderSide(
+                borderSide: const BorderSide(
                   color: Colors.white,
                   width: 2,
                 )
@@ -32,19 +44,14 @@ Widget build (BuildContext context) {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               // Save Button
-              Buttons(context 
-              text: 'Save',
-              onPressed: () {
-                // Save the Task
-              })
+              Button(text: "Save", onPressed: onSave),
+
+              // Space Between The 2 Buttons
+              const SizedBox(width: 8,),
 
               // Cancel Button
-               Buttons(context 
-              text: 'Cancel',
-              onPressed: () {
-                // Cancel the Task
-              })
-            ]
+               Button(text: "Cancel", onPressed: onCancel),
+            ],
           )
 
         ]
